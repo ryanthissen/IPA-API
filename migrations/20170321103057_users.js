40bcf1183dac79ac549fb0f2c82e08294f58dc14
Matt <mmuhr88@gmail.com>
2017-03-21 13:18:13 -0700

    some changes to users migrate/seed file

--- migrations/20170321103057_users.js
+++ migrations/20170321103057_users.js
@@ -5,7 +5,7 @@ exports.up = (knex, Promise) => {
     table.string('first_name').notNullable().defaultTo('');
     table.string('last_name').notNullable().defaultTo('');
     table.string('email').notNullable().unique();
-    table.string('userName').notNullable().unique();
+    table.string('user_name').notNullable().unique();
     table.specificType('hashed_password', 'char(60)').notNullable();
     table.timestamps(true, true);
   });
