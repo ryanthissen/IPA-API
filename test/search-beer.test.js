0271748a084659576f90e14071f69319b2ccf691
Matt <mmuhr88@gmail.com>
2017-03-22 16:25:31 -0700

    got the searchbeers route to work! WOOOOO

--- test/search-beer.test.js
+++ test/search-beer.test.js
@@ -17,7 +17,7 @@ suite('Connection with Brewerydb test', () => {
       abv: "6.9",
       ibu: "77",
       label_url: "https://s3.amazonaws.com/brewerydbapi/beer/PAM6wX/upload_dl9pJu-medium.png",
-      description: "An \"India Pale Ale\" by definition is highly hopped and high in alcohol - you'll find our Stone India Pale Ale to be true to style with a huge hop aroma, flavor and bitterness throughout. If you're a hop-head like us, then you'll love our Stone India Pale Ale! Medium malt character with a heavy dose of over the top hops! Generous \"dry hopping\" gives this beer its abundant hop aroma and crisp hop flavor",
+      description: "An \"India Pale Ale\" by definition is highly hopped and high in alcohol - you'll find our Stone India Pale Ale to be true to style with a huge hop aroma, flavor and bitterness throughout. If you're a hop-head like us, then you'll love our Stone India Pale Ale! Medium malt character with a heavy dose of over the top hops! Generous \"dry hopping\" gives this beer its abundant hop aroma and crisp hop flavor.",
       type_name: "American IPA",
     }, done);
   });
