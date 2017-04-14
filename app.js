d34010ab5a8a5f680b7d986f0e421c3eacbff818
Ryan Thissen <rmt1855@gmail.com>
2017-04-14 14:18:06 -0700

    removed cors middleware

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 const path = require('path');
 app.use(express.static(path.join('public')));
-app.use(cors());
+// app.use(cors());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
