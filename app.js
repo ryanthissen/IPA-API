c23a6c464fbcd6f6c4e21c90abd3868c8c27197a
Ryan Thissen <rmt1855@gmail.com>
2017-04-14 14:15:41 -0700

    added middlware

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const path = require('path');
 app.use(express.static(path.join('public')));
 app.use(cors());
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var config = {
   appRoot: __dirname // required config
